d6b3123212416d07e0ebc416e36a1981d579f2ce
Nguyen Le Minh <nguyenleminh530447@gmail.com>
2020-06-08 17:30:25 +0700

    EAP-01: Add Admin LTE
    - Setup Content, Adminlte and needed files

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,11 +20,15 @@ namespace OnlineHealthInsurance
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/adminlte/plugins/fontawesome-free/css/all.min.css",
+                      "~/adminlte/css/adminlte.min.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/adminlte/js").Include(
+                      "~/adminlte/js/adminlte.min.js"));
         }
     }
 }
